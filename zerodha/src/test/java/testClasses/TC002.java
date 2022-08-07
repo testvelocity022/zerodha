739cd97f1dc1a6fada5df968dc64e19ccf5f5b5f
velocityrepository1234 <velocityrepository@gmail.com>
2022-08-07 21:30:45 +0530

    new script added in TC01 and TC02

--- zerodha/src/test/java/testClasses/TC002.java
+++ zerodha/src/test/java/testClasses/TC002.java
@@ -1,5 +1,19 @@
 package testClasses;
 
-public class TC002 {
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 
+
+
+public class TC125_Buy_sell {
+	@Test
+	public void launcBrowser() {
+		System.setProperty("webdriver.chrome.driver",Configurations.driverPath);
+		driver = new ChromeDriver();	
+		driver.get("https://kite.zerodha.com/");
+		driver.manage().window().maximize();
+		login = new LoginPagePOM(driver);
+		
+	}
+	
 }
