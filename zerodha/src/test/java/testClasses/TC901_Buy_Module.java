87bbe8393bcbe82ee08760d0da53e48a48e3f606
velocityrepository1234 <velocityrepository@gmail.com>
2022-08-14 08:21:59 +0530

    automation script for TC-901 buy module is added

---
+++ zerodha/src/test/java/testClasses/TC901_Buy_Module.java
@@ -0,0 +1,40 @@
+package testClasses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TC901_Buy_Module {
+	//Decalration
+	
+			@FindBy(xpath="//input[@id='email']") private WebElement userName;
+			
+			@FindBy(xpath="//input[@id='pass']") private WebElement password;	
+			
+			@FindBy(xpath="//button[@type='submit']") private WebElement loginBtn;
+			
+			
+			//intialization
+			
+			Admin_POM(WebDriver driver){
+				PageFactory.initElements(driver, this);		
+			}
+			
+			
+			//Usage /actions
+			
+			public void enterUserID(String user) {
+				userName.sendKeys(user);
+			}
+				
+			public void enterPassword(String pass) {
+				password.sendKeys(pass);
+			}
+			
+			public void clickOnLogin() {
+				loginBtn.click();
+			}
+			
+
+}
